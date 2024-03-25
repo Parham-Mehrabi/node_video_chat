d015cf732f6e9b86c086acf0ec70df6fa30ddff1
parham <parham.mehrabi.webdev@gmail.com>
2024-03-25 19:57:27 +0330

    add video to client side

--- public/script.js
+++ public/script.js
@@ -3,6 +3,13 @@ const myPeer = new Peer(undefined, {
     host: '/',
     port: '3001'
 });
+const video_box = document.getElementById('video-box');
+const video = document.createElement('video');
+navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
+    video.srcObject = stream,
+    video.play();
+    video_box.append(video)
+})
 myPeer.on('open', id => {
     socket.emit('join-room', RoomId, id);
 
